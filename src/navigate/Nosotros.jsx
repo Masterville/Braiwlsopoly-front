cdd2c678716feba7c5b511aa4fddd6a051d552ef
masterville <fernando.villela@uc.cl>
2023-07-02 19:33:27 -0400

    fix imgs and icon from nosotros page

--- src/navigate/Nosotros.jsx
+++ src/navigate/Nosotros.jsx
@@ -1,5 +1,10 @@
 import './Nosotros.css'
 import { useState } from 'react'
+import randallImg from '../assets/imgs/avatar.png'
+import fernandoImg from '../assets/imgs/avatar2.png'
+import linkedin from '../assets/imgs/linkedin.png'
+import github from '../assets/imgs/github.png'
+
 
 export default function Nosotros() {
 
@@ -12,7 +17,7 @@ export default function Nosotros() {
             <div className='sub-container'>
             <div className='teams'>
                 <div className='img-container'>
-                    <img src='src/assets/imgs/avatar.png' alt='avatar randall' />
+                    <img src={randallImg} alt='avatar randall' />
                 </div>
                 <div className='name'>Randall Biermann</div>
                 <div className='desig'>Sección 1</div>
@@ -21,15 +26,15 @@ export default function Nosotros() {
                  y la creación de productos innovadores que desafíen la mente. Mi enfoque en la creatividad y la innovación me ha llevado a explorar nuevas ideas y 
                  oportunidades en el futuro. ¡Gracias por conocer un poco más sobre mí!</div>
                 <div className='social-links'>
-                    <a href='#'><img className="icons" src="src\assets\imgs\linkedin.png"/></a>
-                    <a href='#'><img className="icons" src="src\assets\imgs\github.png"/></a>
+                    <a href='#'><img className="icons" src={linkedin}/></a>
+                    <a href='#'><img className="icons" src={github}/></a>
                 </div>
 
             </div>
 
             <div className='teams'>
                 <div className="img-container">
-                    <img src='src/assets/imgs/avatar2.png' alt='avatar fernando' />
+                    <img src={fernandoImg} alt='avatar fernando' />
                 </div>
                 <div className='name'>Fernando Villela</div>
                 <div className='desig'>Sección 1</div>
@@ -37,8 +42,8 @@ export default function Nosotros() {
                  y los videojuegos. Mis favoritos son el Monopoly y el Red Dead Redemption 2 respectivamente. Me motiva que para este proyecto podamos realizar un de ellos y me parece
                  interesante que al convertir el Monopoly en un videojuego se le quite la función de ser el banco a un jugador lo que puede ser algo tediosa a veces. <label htmlFor=""></label></div>
                 <div className='social-links'>
-                    <a href='#'><img className="icons" src="src\assets\imgs\linkedin.png"/></a>
-                    <a href='#'><img className="icons" src="src\assets\imgs\github.png"/></a>
+                    <a href='#'><img className="icons" src={linkedin}/></a>
+                    <a href='#'><img className="icons" src={github}/></a>
                 </div>
                 
             </div>
